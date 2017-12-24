9b909901e62af6a89ace23cb37ac63ee6f72db3a
Torsten Schmits <torstenschmits@gmail.com>
2017-12-25 00:25:12 +0100

    adapt to chromatin 13

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -1,10 +1,15 @@
-from typing import Callable
+import os
 import sys
 import venv
+import logging
 from pathlib import Path
+from typing import Callable
 import subprocess
+from subprocess import PIPE
 
 nvim_inst = None
+amino_log_inst = None
+development = 'AMINO_DEVELOPMENT' in os.environ
 
 
 def nvim(f: Callable) -> None:
@@ -19,40 +24,61 @@ def nvim(f: Callable) -> None:
         f(nvim_inst)
 
 
-def echo(msg: str) -> None:
-    nvim(lambda a: a.command(f'echo "{msg}"'))
-
-
 def amino_logger() -> None:
-    from amino.logging import amino_root_logger, amino_file_logging
-    amino_file_logging(amino_root_logger)
-    return amino_root_logger
+    global amino_log_inst
+    if amino_log_inst is None:
+        from amino.logging import amino_root_logger, amino_root_file_logging
+        amino_root_file_logging()
+        amino_log_inst = amino_root_logger
+    return amino_log_inst
+
+
+def amino_log(msg: str) -> None:
+    try:
+        amino_logger().debug(msg)
+    except Exception as e:
+        pass
+
+
+def echo(msg: str) -> None:
+    amino_log(msg)
+    if not development:
+        nvim(lambda a: a.command(f'echo "{msg}"'))
 
 
 def check_result(result: subprocess.CompletedProcess) -> None:
     msg = str(result).replace('"', '')
     if result.returncode != 0:
-        echo(f'subprocess failed: {msg}')
+        s = f'subprocess failed: {msg}'
+        echo(s)
         sys.exit(1)
 
 
-def install(venv_dir: str, builder: venv.EnvBuilder, bin_path: Path) -> None:
+def subproc(*args: str, pipe: bool=True) -> None:
+    env = dict(AMINO_DEVELOPMENT='1') if development else dict()
+    kw = dict(stdout=PIPE, stderr=PIPE) if pipe else dict()
+    result = subprocess.run(args, env=env, **kw)
+    check_result(result)
+
+
+def create_venv(dir: str) -> None:
+    subproc('python3', '-m', 'venv', str(dir), '--upgrade')
+
+
+def install(venv_dir: str, bin_path: Path) -> None:
     echo('installing chromatin...')
     req = sys.argv[2]
-    builder.create(venv_dir)
     pip = bin_path / 'pip'
-    args = [str(pip), 'install', '-U', '--no-cache', req]
-    result = subprocess.run(args, env=dict(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    check_result(result)
+    subproc(str(pip), 'install', '-U', '--no-cache', req)
 
 
 def start(run: Path, exe: str, bin: str, installed: int) -> None:
-    result = subprocess.run([exe, str(run), exe, bin, str(installed)], env=dict())
-    check_result(result)
+    subproc(exe, str(run), exe, bin, str(installed), pipe=False)
 
 
 def bootstrap() -> None:
     venv_dir = sys.argv[1]
+    create_venv(venv_dir)
     builder = venv.EnvBuilder(system_site_packages=False, with_pip=True)
     ns = builder.ensure_directories(venv_dir)
     bin = Path(ns.bin_path)
@@ -60,7 +86,7 @@ def bootstrap() -> None:
     installed = 0
     if not Path(run).exists():
         installed = 1
-        install(venv_dir, builder, bin)
+        install(venv_dir, bin)
         echo('initializing chromatin...')
     start(run, ns.env_exe, ns.bin_path, installed)
 
@@ -68,9 +94,10 @@ try:
     bootstrap()
     sys.exit(0)
 except Exception as e:
+    msg = f'error while bootstrapping chromatin: {e}'
     try:
-        echo(f'error while bootstrapping chromatin: {e}')
+        echo(msg)
         amino_logger().caught_exception_error('bootstrapping chromatin', e)
     except Exception:
-        pass
+        print(msg, file=sys.stderr)
     sys.exit(1)
